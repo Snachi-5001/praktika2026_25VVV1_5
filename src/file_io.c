e12c5f1e6065fa268b344efbccd142158bf90431
Zinka <Molchanov.daniil58@gmail.com>
2026-06-28 00:19:09 +0300

    Реализованы файловые операции

--- src/file_io.c
+++ src/file_io.c
@@ -1,5 +1,71 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-void save_to_file(int *arr, int n, const char *filename);
-void read_from_file(int *arr, int n, const char *filename);
-#endif
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "file_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int* load_file(const char* filename, int* size)
+{
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+	{
+        printf("Ошибка открытия файла\n");
+        return NULL;
+    }
+
+    int cnt = 0;
+    int temp;
+    while (fscanf(file, "%d,", &temp) == 1)
+	{
+        cnt++;
+    }
+
+    
+    if (cnt == 0)
+	{
+        printf("Файл пуст\n");
+        fclose(file);
+        return NULL;
+    }
+
+    rewind(file);
+    int* mass = (int*)malloc(cnt * sizeof(int));
+    *size = cnt;
+
+    for (int i = 0; i < cnt; i++)
+	{
+        fscanf(file, "%d,", &mass[i]);
+    }
+
+    fclose(file);
+    return mass;
+}
+
+void save_file(const char* filename, int* mass, int n)
+{
+    if (n == 0)
+	{
+        printf("Массив пуст\n");
+        return;
+    }
+
+    FILE* file = fopen(filename, "w");
+    if (file == NULL)
+	{
+        printf("Ошибка создания\n");
+        return;
+    }
+
+    
+    for (int i = 0; i < n; i++)
+	{
+        fprintf(file, "%d", mass[i]);
+        if (i < n - 1)
+		{
+            fprintf(file, ",");
+        }
+    }
+
+    fclose(file);
+    printf("Сохранено в %s\n", filename);
+}
